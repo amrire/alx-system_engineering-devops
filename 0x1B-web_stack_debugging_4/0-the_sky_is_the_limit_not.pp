ffdd233769acb69cd22035ed775a3c26a2306cf4
x86Skwizer <yassineamrire@protonmail.com>
2024-03-14 21:27:24 +0000

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,9 @@
 #Web stack debugging
+
+#Increase number of default file
 exec {'fix--for-nginx':
-	command	=> "sed -i 's/15/unlimited/' /etc/default/nginx && service nginx restart",
-	path => '/usr/bin:/bin'
+  #Script fix
+  command => "sed -i 's/15/unlimited/' /etc/default/nginx && service nginx restart",
+  #Paths to cmds
+  path => '/usr/bin:/bin:/usr/local/bin/',
 }
